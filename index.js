6f101d6d7a2a3dcf4e90369c33177c9e368c3d94
Naqeeb Malik <naqeeb0004@gmail.com>
2018-06-30 20:20:28 +0100

    rest of initial files

---
+++ index.js
@@ -0,0 +1,146 @@
+var express = require('express');
+var socket = require('socket.io');
+
+var app = express();
+var server = app.listen(8080, () => {
+  console.log('listening on port 8080');
+});
+
+app.use(express.static('public'));
+
+var io = socket(server);
+
+let snakes = [];
+let food = [];
+let messages = [];
+const tiles = 48;
+const tileSize = 16;
+const offset = 0.1;
+const foodPerSnek = 5;
+const moveScale = 0.5;
+const defaultLength = 64;
+
+io.on('connection', con => {
+  console.log(con.id, 'made connection');
+  snakes.push({
+    id: con.id,
+    blocks: [
+      {
+        x: Math.floor(Math.random() * tiles),
+        y: Math.floor(Math.random() * tiles)
+      }
+    ],
+    dx: 1,
+    dy: 0,
+    len: defaultLength,
+    col: `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() *
+      255})`,
+    message: null,
+    dead: false
+  });
+  for (let i = 0; i < foodPerSnek; i++) {
+    food.push({
+      x: Math.floor(Math.random() * tiles),
+      y: Math.floor(Math.random() * tiles),
+      size: tileSize
+    });
+  }
+
+  con.on('move', data => {
+    const snake = snakes.filter(snake => snake.id === con.id)[0];
+    switch (data) {
+      case 'u':
+        snake.dx = 0;
+        snake.dy = -1;
+        break;
+      case 'l':
+        snake.dx = -1;
+        snake.dy = 0;
+        break;
+      case 'd':
+        snake.dx = 0;
+        snake.dy = 1;
+        break;
+      case 'r':
+        snake.dx = 1;
+        snake.dy = 0;
+        break;
+      default:
+        break;
+    }
+  });
+  con.on('disconnecting', reason => {
+    console.log(con.id, 'disconnecting');
+    snakes = snakes.filter(snake => snake.id !== con.id);
+  });
+  con.on('message', data => {
+    const newMsg = {
+      id: con.id,
+      col: snakes.filter(snake => snake.id === con.id)[0].col,
+      message: data,
+      life: 100
+    };
+    messages.push(newMsg);
+    snake = snakes.filter(snake => snake.id === con.id)[0];
+    snake.message = newMsg;
+    console.log('>>', con.id, data);
+  });
+});
+
+setInterval(() => {
+  // console.log(`snakes: ${snakes.length}`);
+  snakes.forEach(snake => {
+    if (!snake.dead) {
+      const newX =
+        (snake.blocks[snake.blocks.length - 1].x +
+          snake.dx * moveScale +
+          tiles) %
+        tiles;
+      const newY =
+        (snake.blocks[snake.blocks.length - 1].y +
+          snake.dy * moveScale +
+          tiles) %
+        tiles;
+      food.forEach(fud => {
+        if (fud.x === newX && fud.y === newY) {
+          snake.len++;
+          if (snakes.length * foodPerSnek < food.length) {
+            food = food.filter(iFud => iFud.x !== fud.x && iFud.y !== fud.y);
+          } else {
+            fud.x = Math.floor(Math.random() * tiles);
+            fud.y = Math.floor(Math.random() * tiles);
+          }
+        }
+      });
+      snake.blocks.push({ x: newX, y: newY });
+      while (snake.blocks.length > snake.len) {
+        snake.blocks.splice(0, 1);
+      }
+      let offset = Math.min(4, snake.blocks.length);
+      for (let i = 0; i < snake.blocks.length; i++) {
+        snake.blocks[snake.blocks.length - 1 - i].size = tileSize - offset;
+        if (tileSize - offset > 8) {
+          offset += 0.64;
+        }
+      }
+    }
+  });
+
+  let data = {
+    snakes,
+    food,
+    messages,
+    meta: { tiles, tileSize, offset }
+  };
+
+  messages.forEach(msg => {
+    if (msg.life > 0) {
+      msg.life--;
+      if (msg.life == 0) {
+        snakes.filter(snake => snake.id === msg.id)[0].message = null;
+      }
+    }
+  });
+
+  io.sockets.emit('data', data);
+}, 1000 / 24);
---
+++ public/snek.js
@@ -0,0 +1,174 @@
+const canv = document.getElementById('canv');
+const ctx = canv.getContext('2d');
+let socket = null;
+let snakes = [];
+let food = [];
+let messages = [];
+let chat = { showing: false, message: null };
+
+function connectToServer() {
+  console.log('connecting to', document.getElementById('serverIp').value);
+  socket = io.connect(document.getElementById('serverIp').value);
+  if (socket !== null) {
+    // document.getElementById('game').style.display = 'block';
+    document.getElementById('serverconnectbuis').style.display = 'none';
+    callback();
+  }
+}
+function callback() {
+  ctx.fillRect(0, 0, canv.width, canv.height);
+
+  socket.on('data', data => {
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    ctx.fillStyle = 'grey';
+    ctx.translate(200, 0);
+    snakes = data.snakes;
+    food = data.food;
+    messages = data.messages;
+    // canvas scene
+    ctx.clearRect(0, 0, canv.width, canv.height);
+    ctx.fillRect(0, 0, canv.width, canv.height);
+    ctx.translate(2, 2);
+    drawGrid(ctx, data.meta.tiles, data.meta.tileSize, data.meta.offset);
+    snakes.forEach(snake => {
+      drawSnake(
+        ctx,
+        snake,
+        data.meta.tiles,
+        data.meta.tileSize,
+        data.meta.offset
+      );
+      if (snake.message !== null) {
+        ctx.fillStyle = `rgba(0,0,0,${snake.message.life / 50})`;
+        ctx.font = '16px arial';
+        ctx.fillText(
+          `${snake.message.message}`,
+          snake.blocks[snake.blocks.length - 1].x * data.meta.tileSize,
+          snake.blocks[snake.blocks.length - 1].y * data.meta.tileSize
+        );
+      }
+    });
+    food.forEach(nomnom =>
+      drawFood(ctx, nomnom, data.meta.tileSize, data.meta.offset)
+    );
+    // scores
+    for (let i = 0; i < snakes.length; i++) {
+      ctx.fillStyle = snakes[i].col;
+      ctx.fillRect(-180, 10 + i * 40, 20, 20);
+      ctx.fillStyle = 'green';
+      ctx.fillText(snakes[i].len, -155, 10 + i * 40 + 16);
+    }
+    ctx.translate(-202, -2);
+    // messages
+    let msgsBox = document.getElementById('msgs');
+    msgsBox.innerHTML = '';
+    let prevCol = null;
+    messages.forEach(msg => {
+      if (prevCol !== msg.col) {
+        prevCol = msg.col;
+        msgsBox.innerHTML += '<br/>';
+      }
+      msgsBox.innerHTML += `<div style='border-left:4px solid ${msg.col}'>   ${
+        msg.message
+      }</div>`;
+    });
+    msgsBox.scrollTo(0, 500);
+    if (chat.showing) {
+      ctx.fillStyle = 'darkgrey';
+      ctx.fillRect(202, canv.height - 30, canv.width - 204, 2);
+      ctx.fillStyle = 'rgba(0,0,0,0.2)';
+      ctx.fillRect(202, canv.height - 30, canv.width - 204, 28);
+      ctx.fillStyle = 'black';
+      ctx.font = '12px monospace';
+      ctx.fillText(chat.message, 210, canv.height - 10);
+    }
+  });
+}
+
+function drawGrid(ctx, tiles, tileSize = 8, offset = 1, col = 'white') {
+  for (let x = 0; x < tiles; x++) {
+    for (let y = 0; y < tiles; y++) {
+      ctx.fillStyle = col;
+      ctx.fillRect(
+        x * tileSize,
+        y * tileSize,
+        tileSize - offset,
+        tileSize - offset
+      );
+    }
+  }
+}
+
+function drawSnake(ctx, snakeObj, tiles, tileSize = 8, offset = 1) {
+  ctx.fillStyle = snakeObj.col;
+  snakeObj.blocks.forEach(block => {
+    ctx.fillStyle = snakeObj.col;
+    ctx.beginPath();
+    ctx.arc(
+      block.x * tileSize + tileSize / 2,
+      block.y * tileSize + tileSize / 2,
+      block.size / 2 - offset,
+      0,
+      2 * Math.PI
+    );
+    ctx.closePath();
+    ctx.fill();
+  });
+}
+
+function drawFood(ctx, food, tileSize = 8, offset = 1) {
+  ctx.fillStyle = 'red';
+  ctx.fillRect(
+    food.x * tileSize,
+    food.y * tileSize,
+    tileSize - offset,
+    tileSize - offset
+  );
+}
+
+window.addEventListener('keydown', e => {
+  if (!chat.showing) {
+    switch (e.key) {
+      case 'w':
+        socket.emit('move', 'u');
+        break;
+      case 'a':
+        socket.emit('move', 'l');
+        break;
+      case 's':
+        socket.emit('move', 'd');
+        break;
+      case 'd':
+        socket.emit('move', 'r');
+        break;
+      case 'Enter':
+        chat.showing = true;
+        chat.message = '';
+        break;
+      default:
+        break;
+    }
+  } else {
+    if (e.keyCode === 8) {
+      chat.message = chat.message.slice(0, chat.message.length - 1);
+    } else if (e.keyCode === 13) {
+      socket.emit('message', chat.message);
+      chat.message = null;
+      chat.showing = false;
+    } else if (e.keyCode === 27) {
+      chat.message = null;
+      chat.showing = false;
+    } else {
+      if (
+        (chat.message.length < 64 && (e.keyCode > 47 && e.keyCode < 58)) ||
+        e.keyCode == 32 ||
+        (e.keyCode > 64 && e.keyCode < 91) ||
+        (e.keyCode > 95 && e.keyCode < 112) ||
+        (e.keyCode > 185 && e.keyCode < 193) ||
+        (e.keyCode > 218 && e.keyCode < 223)
+      ) {
+        chat.message += e.key;
+      }
+    }
+  }
+});
