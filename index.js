ac4664c4f67d59e9fd95bb8ab3a1c64a11c3e476
Naqeeb Malik <naqeeb0004@gmail.com>
2018-07-29 19:36:52 +0100

    changed dir to include __dirname

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var server = app.listen(8080, () => {
   console.log('listening on port 8080');
 });
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 var io = socket(server);
 
